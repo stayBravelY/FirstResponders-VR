310db969b67618a44afa7d261fb021e868b9782b
stayBravelY <53599037+stayBravelY@users.noreply.github.com>
2021-10-11 02:08:03 +0800

    Add basic raycaster

---
+++ VR demonstrator for first responders/Assets/Scripts/Raycaster.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raycaster : MonoBehaviour
+{
+
+
+    void Update()
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit)) {
+            Debug.DrawLine (ray.origin, hit.point, Color.red);
+            print(hit.collider.gameObject.name);
+        }else
+        {
+            Debug.DrawLine (ray.origin, ray.origin + ray.direction * 100, Color.blue);
+        }
+       
+
+
+    }
+}
