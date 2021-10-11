1c4b2a7cbc93cc2677643d5a752a748469be9b04
stayBravelY <53599037+stayBravelY@users.noreply.github.com>
2021-10-11 21:45:58 +0800

    raycaster adjustment

--- VR demonstrator for first responders/Assets/Scripts/Raycaster.cs
+++ VR demonstrator for first responders/Assets/Scripts/Raycaster.cs
@@ -1,19 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Raycaster : MonoBehaviour
 {
-
+    private GameObject RaycastObj;
 
     void Update()
     {
         Ray ray = new Ray(transform.position, transform.forward);
         RaycastHit hit;
+        
 
         if (Physics.Raycast(ray, out hit)) {
             Debug.DrawLine (ray.origin, hit.point, Color.red);
             print(hit.collider.gameObject.name);
+            RaycastObj = hit.collider.gameObject;
+            
+            if (Input.GetKeyDown("e")) {
+                Debug.Log("You've interacted with it");
+                RaycastObj.SetActive(false);
+            }
         }else
         {
             Debug.DrawLine (ray.origin, ray.origin + ray.direction * 10, Color.blue);
@@ -22,4 +30,6 @@ public class Raycaster : MonoBehaviour
 
 
     }
+
+    
 }
