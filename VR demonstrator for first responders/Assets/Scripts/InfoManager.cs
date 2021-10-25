ddd6cd801965c90ff5d3dcc2f1572e724d1a3804
stayBravelY <53599037+stayBravelY@users.noreply.github.com>
2021-10-26 01:00:31 +0800

    pop up the info box when gaze the object

---
+++ VR demonstrator for first responders/Assets/Scripts/InfoManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfoManager : MonoBehaviour
+{
+    const float SPEED = 6f;
+    [SerializeField]
+    Transform InfoBox;
+
+    Vector3 ChangedScale = Vector3.zero;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        InfoBox.localScale = Vector3.Lerp(InfoBox.localScale,ChangedScale, Time.deltaTime * SPEED);
+    }
+
+    public void OpenInfo(){
+        ChangedScale = Vector3.one;
+    }
+    public void CloseInfo(){
+        ChangedScale = Vector3.zero;
+    }
+}
--- VR demonstrator for first responders/Assets/Scripts/Raycaster.cs
+++ VR demonstrator for first responders/Assets/Scripts/Raycaster.cs
@@ -1,32 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Raycaster : MonoBehaviour
 {
     private GameObject RaycastObj;
+    List<InfoManager> infos = new List<InfoManager>();
 
+    void Start() {
+        infos = FindObjectsOfType<InfoManager>().ToList();
+    }
     void Update()
     {
         Ray ray = new Ray(transform.position, transform.forward);
         RaycastHit hit;
         
 
-        if (Physics.Raycast(ray, out hit)) {
+        if (Physics.Raycast(ray, out hit, 5)) {
             Debug.DrawLine (ray.origin, hit.point, Color.red);
             print(hit.collider.gameObject.name);
-            RaycastObj = hit.collider.gameObject;
+            //RaycastObj = hit.collider.gameObject;
+            GameObject go = hit.collider.gameObject;
+            if (go.CompareTag("Interactable")){
+                OpenInfo(go.GetComponent<InfoManager>());
+            }
             
-         
         }else
         {
             Debug.DrawLine (ray.origin, ray.origin + ray.direction * 10, Color.blue);
+            CloseAll();
         }
        
-
+    
 
     }
+    void OpenInfo(InfoManager desiredInfo){
+        foreach (InfoManager info in infos) {
+            if (info == desiredInfo){
+                info.OpenInfo();
+            }
+            else{
+                info.CloseInfo();
+            }
+        }
+    }
+    void CloseAll(){
+        foreach (InfoManager info in infos) {
+            info.CloseInfo();
+        }
+    }
+
+        
 
     
 }
